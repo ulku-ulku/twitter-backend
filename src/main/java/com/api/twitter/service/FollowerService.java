e9e8b387a54fa6d23765a5335bcd19f1919fb017
ulku-ulku <ulkuulku124@gmail.com>
2025-07-11 07:59:25 +0300

    v

--- src/main/java/com/api/twitter/service/FollowerService.java
+++
@@ -1,7 +0,0 @@
-package com.api.twitter.service;
-
-public interface FollowerService {
-    void follow(String followerUserId, String followeeUserId) throws Exception;
-
-    void unfollow(String followerUserId, String followeeUserId) throws Exception;
-}
--- src/main/java/com/api/twitter/service/LikeService.java
+++
@@ -1,7 +0,0 @@
-package com.api.twitter.service;
-
-public interface LikeService {
-    void like(String postId, String userId);
-
-    void removeLike(String postId, String userId);
-}
